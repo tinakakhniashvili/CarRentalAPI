013bc355670be4c04f7a32d2af8dc338feb9988f
tinakakhniashvili <hosktina@gmail.com>
2025-03-13 17:02:23 +0400

    Implement UserController.cs

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,8 +1,45 @@
+using CarRentalApp.DTOs;
+using CarRentalApp.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarRentalApp.Controllers;
 
+[ApiController]
+[Route("api/[controller]")]
 public class UserController : ControllerBase
 {
-    
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [Authorize]
+    [HttpGet("{id}")]
+    public IActionResult GetUserById(int id)
+    {
+        var user = _userService.GetUserById(id);
+        if (user == null)
+        {
+            return NotFound(new { message = "User not found" });
+        }
+
+        return Ok(user);
+    }
+
+    [Authorize]
+    [HttpPut("{id}")]
+    public IActionResult UpdateUser(int id, [FromBody] UserDTO userDTO)
+    {
+        var user = _userService.GetUserById(id);
+        if (user == null)
+        {
+            return NotFound(new { message = "User not found" });
+        }
+
+        var updatedUser = _userService.UpdateUser(id, userDTO);
+        return Ok(updatedUser);
+    }
 }
