fde4f899056b54099ca09c63c4bfeeaf0435f69b
tinakakhniashvili <hosktina@gmail.com>
2025-03-13 13:25:27 +0400

    Impelement CarService.cs and UserService.cs

--- Services/CarService.cs
+++ Services/CarService.cs
@@ -1,22 +1,50 @@
 using CarRentalApp.DTOs;
 using CarRentalApp.Interfaces;
+using CarRentalApp.Interfaces.Repositories;
 
 namespace CarRentalApp.Services;
 
 public class CarService : ICarService
 {
+    private readonly ICarRepository _carRepository;
+
+    public CarService(ICarRepository carRepository)
+    {
+        _carRepository = carRepository;
+    }
     public IEnumerable<CarDTO> GetAllCars()
     {
-        throw new NotImplementedException();
+        var cars = _carRepository.GetAllCars();
+        return cars.Select(car => new CarDTO
+        {
+            Id = car.Id,
+            Make = car.Make,
+            Model = car.Model,
+            Year = car.Year,
+            PricePerDay = car.PricePerDay
+        }).ToList();
     }
 
     public CarDTO GetCarById(int carId)
     {
-        throw new NotImplementedException();
+        var car = _carRepository.GetCarById(carId);
+        if (car == null) return null;
+
+        return new CarDTO
+        {
+            Id = car.Id,
+            Make = car.Make,
+            Model = car.Model,
+            Year = car.Year,
+            PricePerDay = car.PricePerDay
+        };
     }
 
     public void DeleteCar(int carId)
     {
-        throw new NotImplementedException();
+        var car = _carRepository.GetCarById(carId);
+
+        if (car != null)
+            _carRepository.DeleteCar(carId);
     }
 }
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,17 +1,45 @@
 using CarRentalApp.DTOs;
 using CarRentalApp.Interfaces;
+using CarRentalApp.Interfaces.Repositories;
 
 namespace CarRentalApp.Services;
 
 public class UserService : IUserService
 {
+    private readonly IUserRepository _userRepository;
+
+    public UserService(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
     public UserDTO GetUserById(int userId)
     {
-        throw new NotImplementedException();
+        var user = _userRepository.GetUserById(userId);
+        if (user == null) return null;
+
+        return new UserDTO
+        {
+            Id = user.Id,
+            FullName = user.FullName,
+            Email = user.Email
+        };
     }
 
     public UserDTO UpdateUser(int userId, UserDTO userDTO)
     {
-        throw new NotImplementedException();
+        var user = _userRepository.GetUserById(userId);
+        if (user == null) return null;
+
+        user.FullName = userDTO.FullName;
+        user.Email = userDTO.Email;
+        
+        _userRepository.UpdateUser(user);
+
+        return new UserDTO
+        {
+            Id = user.Id,
+            FullName = user.FullName,
+            Email = user.Email
+        };
     }
 }
